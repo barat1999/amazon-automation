1b60b7b69c46405f8b13b7b1531e191292c5e761
barat1999 <gopibaratnadar00@gmail.com>
2024-08-09 23:45:53 +0530

    fix for clicking elements

--- src/test/java/objects/CartPage.java
+++ src/test/java/objects/CartPage.java
@@ -25,7 +25,7 @@ public class CartPage extends PageObject{
     public boolean verifyProductInCart() throws Exception {
         try{
             ProductPage ProductPage = new ProductPage(driver);
-            clickJs(goToCartButton);
+            click(goToCartButton);
             waitUntilPresent(productNameInCart);
             String currentUrl = getCurrentPageUrl(driver);
             Logger.info("Current Page Url - " + currentUrl);
